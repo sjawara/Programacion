398ed5bf239f3289befd96429d49f686741f5857
sjawara <113584527+sjawara@users.noreply.github.com>
2022-11-11 12:30:40 +0100

    Add files via upload

---
+++ UF1/PR01/src/cat/institutmvm/PR01.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package cat.institutmvm;
+
+/**
+ *
+ * @author alumne_1r
+ */
+public class PR01 {
+    //Pre:
+    
+    private static final int MIN1=111;
+    private static final int MAX1=999;
+    private static final int MIN2=14;
+    private static final int MAX3=120;
+    private static final int TIPOV=0;
+    private static final int TIPOV2=3;
+    private static final int ICOMPRA=100;
+    private static final int TLFMIN=111111111;
+    private static final int TLFMAX=999999999;
+    
+     private static final String MSG_1="Introduce tú id.\n"
+             + "(ha de ser un numero entero de 111 a 999 (ambos incluidos).)";
+     private static final String MSG_2="Introduce tú edad.\n"
+             + "(Recuerda que has de tener más de 14 años y menos de 120)";
+     private static final String MSG_3="Cual es el tipo de venta?\n"
+             + "(0)Venta libre\n"
+             + "(1)Pensionista"
+             + "(2)Carnet joven"
+             + "(3)Socio";
+     private static final STRING MSG_4="";
+    public static void main(String[] args) {
+        
+        
+    }
+    
+}
