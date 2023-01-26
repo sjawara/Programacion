ae9e10faf87c6133f8d5673ff9ae62e49f783df2
sjawara <113584527+sjawara@users.noreply.github.com>
2023-01-26 14:31:07 +0100

    Add files via upload

--- UF1/UF2/src/cat/institutmvm/app/Main.java
+++ UF1/UF2/src/cat/institutmvm/app/Main.java
@@ -12,17 +12,52 @@ import java.util.Scanner;
  * @author alumne_1r
  */
 public class Main {
-private static final String MSG_3="Introduce un texto";
-private static final String MSG_1="El texto que has escrito es el siguiente:";
-private static final String MSG_2="La cantidad de caracteres es:";
-private static final String MSG_4="Introduce el texto a buscar";
-private static final String MSG_5="Introduce un caracter";
+
+    private static final int EJ = 4;
+    private static final String MSG_3 = "Introduce un texto";
+    private static final String MSG_1 = "El texto que has escrito es el siguiente:";
+    private static final String MSG_2 = "La cantidad de caracteres es:";
+    private static final String MSG_4 = "Introduce el texto a buscar";
+    private static final String MSG_5 = "Introduce un caracter";
+    private static final String MSG_6 = "Introdueix un nombre natural";
+    private static final String MSG_7 = "Error,ha de ser un nombre natural";
+
     public static void main(String[] args) {
+        /*int num = 0, num2 = 0, num1 = 0;
+        //int[] arrayEj9 = new int[EJ];
+        boolean tipusCorrecte = false, validacio;
+        Scanner sc = new Scanner(System.in);
+        String textoIntroducido = "";
+        */
         Entrada ent = new Entrada();
-        Scanner sc= new Scanner(System.in);
-        String textoIntroducido="";
+        /*Ejercicio 7
+         num=ent.validacioInt(0, MSG_6, MSG_7, tipusCorrecte, sc);
+        System.out.println(Math.abs(num));
         
-        
+        //Ejercicio 8
+        num = ent.validacioInt(num, MSG_6, MSG_7, tipusCorrecte, sc);
+        num2 = ent.validacioInt(num2, MSG_6, MSG_7, tipusCorrecte, sc);
+        System.out.println(Math.pow(num,num2));
+         */
+        /*Ejercicio 9
+        for(int i=0;i<EJ;i++){
+            arrayEj9[i]=ent.validacioInt(num, MSG_6, MSG_7, tipusCorrecte, sc);
+        }
+        for(int i=1;i<EJ;i=i+2){
+            System.out.println(arrayEj9[i]);
+        }
+         */
+        //Ejercicio 10
+
+        /*
+        //Ejercicio 13
+        num = ent.validacioInt(num, MSG_6, MSG_7, tipusCorrecte, sc);
+        num1 = ent.validacioInt(num, MSG_6, MSG_7, tipusCorrecte, sc);
+        num2 = ent.validacioInt(num, MSG_6, MSG_7, tipusCorrecte, sc);
+        validacio=ent.validacioRang(num, num1, num2);
+        System.out.format((num <= num2 && num >= num1) ? "%d esta dins del rang "
+                + "%d i %d" : "%d no esta dins del rang %d i %d",num,num1,num2);
+         */
         /*Ejercicio 16.
         System.out.println(MSG_3);
         String textoIntroducido = ent.devolverTexto();
@@ -41,7 +76,7 @@ private static final String MSG_5="Introduce un caracter";
         System.out.println("El texto "+(textoIntroducido.contains(contiene)?"contiene:":"no contiene: ")+ contiene);
         
         //Ejercicio 19
-        */
+        
         
         
         //Ejericicio 20
@@ -52,11 +87,28 @@ private static final String MSG_5="Introduce un caracter";
         System.out.println(MSG_5);
         char caracter2=sc.next().charAt(0);
         System.out.println(textoIntroducido.replace(caracter, caracter2));
+         */
+        //System.out.println(ent.fibonacci(4));
         
+        int n = 3;
+        ent.hanoiTower(n, 'A', 'C', 'B');
+        /*
+        int arr[] = {10,20,30,40,50};  
+        int key = 50;  
+        int last=arr.length-1;  
+        int result;
+        result=ent.binarySearch(arr, 0, last, key);
         
-        
-        
-        
+        if (result == -1){ 
+            System.out.println("Element is not found!");}  
+        else{
+            System.out.println("Element is found at index: "+result);
+                    }
+        */
+    }  
     }
-    
-}
+
+          
+
+
+
--- UF1/UF2/src/cat/institutmvm/utils/Entrada.java
+++ UF1/UF2/src/cat/institutmvm/utils/Entrada.java
@@ -11,35 +11,77 @@ import java.util.Scanner;
  * @author alumne_1r
  */
 public class Entrada {
-    private static final String MSG_1="Introdueix un nombre natural";
-    private static final String MSG_2="Error,ha de ser un nombre natural";
-    public int validacioInt(int num){
-        boolean tipusCorrecte;
-        Scanner sc= new Scanner(System.in);
-        System.out.println(MSG_1);
-        tipusCorrecte=sc.hasNextInt();
-        if(tipusCorrecte){
-            num=sc.nextInt();
-        }
-        else{
-            System.out.println(MSG_2);
-            sc.next();
-        }
-        sc.close();
+
+    public int validacioInt(int num, String MSG_6, String MSG_7, boolean tipusCorrecte, Scanner sc) {
+        do {
+            System.out.println(MSG_6);
+            tipusCorrecte = sc.hasNextInt();
+            if (tipusCorrecte) {
+                num = sc.nextInt();
+            } else {
+                System.out.println(MSG_7);
+                sc.next();
+            }
+        } while (!tipusCorrecte);
         return num;
     }
-    
-    public String devolverTexto(Scanner sc,String textoIntroducido){
-        textoIntroducido=sc.nextLine();
+
+    public String devolverTexto(Scanner sc, String textoIntroducido) {
+        textoIntroducido = sc.nextLine();
         return textoIntroducido;
     }
-    
-    public int contarCaracteres(){
-    Scanner sc= new Scanner (System.in);
-    String textoIntroducido;
-        textoIntroducido=sc.nextLine();
-        int cantCaracteres =textoIntroducido.length();
+
+    public int contarCaracteres(Scanner sc) {
+
+        String textoIntroducido;
+        textoIntroducido = sc.nextLine();
+        int cantCaracteres = textoIntroducido.length();
         return cantCaracteres;
     }
-    
+
+    public boolean validacioRang(int num, int num1, int num2) {
+        int aux;
+        boolean validacio;
+        if (num1 > num2) {
+            aux = num2;
+            num2 = num1;
+            num1 = aux;
+        }
+        validacio = (num <= num2 && num >= num1) ? true : false;
+        return validacio;
+    }
+
+    public int fibonacci(int num) {
+        //condició base
+        if (num == 0 || num == 1) {
+            return num;
+        } else {
+            return fibonacci(num - 1) + fibonacci(num - 2);
+        }
+    }
+
+    public void hanoiTower(int n, char from_disc, char to_disc, char aux) {
+        if (n == 1) {
+            System.out.format("Mou el disc 1 des de %c fins a %c\n", from_disc, to_disc);
+        } else {
+            hanoiTower(n - 1, from_disc, aux, to_disc);
+            System.out.format("Mou el disc %d des de %c fins a %c\n", n, from_disc, to_disc);
+            hanoiTower(n - 1, aux, to_disc, from_disc);
+        }
+    }
+
+    public  int binarySearch(int arr[], int first, int last, int key) {
+        if (last >= first) {
+            int mid = first + (last - first) / 2;
+            if (arr[mid] == key) {
+                return mid;
+            }
+            if (arr[mid] > key) {
+                return binarySearch(arr, first, mid - 1, key);//search in left subarray  
+            } else {
+                return binarySearch(arr, mid + 1, last, key);//search in right subarray  
+            }
+        }
+        return -1;
+    }
 }
