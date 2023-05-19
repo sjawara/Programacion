dfd062765f0929365cc67e837fa373f3b97af9a8
sjawara <113584527+sjawara@users.noreply.github.com>
2023-05-19 10:59:55 +0200

    Add files via upload

--- Projecte2_UF6/src/cat/institutmvm/ui/Prj_GUI.java
+++ Projecte2_UF6/src/cat/institutmvm/ui/Prj_GUI.java
@@ -38,8 +38,8 @@ public class Prj_GUI extends JFrame{
      JTextField getDni=new JTextField(33);
      JButton comprovació=new JButton("Comprovació");
      txtDni.setBounds(10,13,50, 30);
-     getDni.setBounds(10, 42, 360, 20);
-     comprovació.setBounds(370, 42, 120, 19);
+     getDni.setBounds(10, 38, 360, 20);
+     comprovació.setBounds(370, 38, 120, 19);
      panel.add(txtDni);
      panel.add(getDni);
      panel.add(comprovació);
@@ -47,16 +47,16 @@ public class Prj_GUI extends JFrame{
      
      JLabel nomCognoms = new JLabel("Nom y cognoms:");
      JTextField getNomCognoms = new JTextField(38);
-     nomCognoms.setBounds(10, 53, 100, 30);
-     getNomCognoms.setBounds(10, 80,480, 20);
+     nomCognoms.setBounds(10, 53, 100, 20);
+     getNomCognoms.setBounds(10, 73,465, 20);
      getNomCognoms.setEditable(false);
      panel.add(nomCognoms);
      panel.add(getNomCognoms);
      
      JLabel TSI = new JLabel("TSI:");
      JTextField getTSI = new JTextField(38);
-     TSI.setBounds(10,90,100,30);
-     getTSI.setBounds(10, 115, 480, 20);
+     TSI.setBounds(10,83,100,30);
+     getTSI.setBounds(10, 105, 465, 20);
      getTSI.setEditable(false);
      panel.add(TSI);
      panel.add(getTSI);
@@ -64,8 +64,8 @@ public class Prj_GUI extends JFrame{
      JLabel data_naixement = new JLabel("Data naixement:");
      JTextField getData_naixement = new JTextField(38);
      getData_naixement.setEditable(false);
-     data_naixement.setBounds(10, 125, 100, 30);
-     getData_naixement.setBounds(10, 150, 480, 20);
+     data_naixement.setBounds(10, 117, 100, 30);
+     getData_naixement.setBounds(10, 140, 465, 20);
      
      panel.add(data_naixement);
     panel.add(getData_naixement);
@@ -73,8 +73,8 @@ public class Prj_GUI extends JFrame{
     
     JLabel edat = new JLabel("Edat");
     JTextField getEdat = new JTextField();
-    edat.setBounds(10, 165, 100, 30);
-    getEdat.setBounds(10, 190, 480, 20);
+    edat.setBounds(10, 150, 100, 30);
+    getEdat.setBounds(10, 175, 465, 20);
     getEdat.setEditable(false);
     panel.add(edat);
     panel.add(getEdat);
@@ -82,14 +82,24 @@ public class Prj_GUI extends JFrame{
     JLabel gènere = new JLabel("Gènere");
     JTextField getGènere = new JTextField();
     getGènere.setEditable(false);
-    gènere.setBounds(10, 205, 100, 30);
-    getGènere.setBounds(10, 230, 480, 20);
+    gènere.setBounds(10, 185, 100, 30);
+    getGènere.setBounds(10, 210, 465, 20);
     panel.add(gènere);
     panel.add(getGènere);
     
     JLabel motiu = new JLabel("Motiu de la urgència");
     JTextArea motiuVisita = new JTextArea();
+    motiu.setBounds(10,220,150,30);
+    motiuVisita.setBounds(10, 245, 465, 100);
+    panel.add(motiu);
+    panel.add(motiuVisita);
     
+    JButton afegir = new JButton("1");
+    JButton mostrar = new JButton("2");
+    JButton estadístiques = new JButton("3");
+    JButton fitxer = new JButton("4");
+    
+    afegir.setBounds(50, WIDTH, WIDTH, HEIGHT);
     
     
     getContentPane().add(panel);
